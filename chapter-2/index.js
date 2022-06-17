2b63b9b8764f165b2ecaa82790dbc7c4992346ce
bbawuah <brianbawuah@icloud.com>
2022-06-17 16:28:25 +0200

    first commit

--- chapter-2/index.js
+++ chapter-2/index.js
@@ -1,7 +1,10 @@
 var Scene = /** @class */ (function () {
     function Scene() {
-        this.vertexShader = "\n  attribute vec4 a_Position;\n\n  void main() {\n    gl_Position = a_Position;\n    gl_PointSize = 10.0;\n  }\n  ";
-        this.fragmentShader = "\n  void main() {\n    gl_FragColor = vec4(1.0,0.0,1.0,1.0);\n  }\n  ";
+        var _this = this;
+        this.vertexShader = "\n  attribute vec4 a_Position;\n  attribute float a_PointSize;\n\n  void main() {\n    gl_Position = a_Position;\n    gl_PointSize = a_PointSize;\n  }\n  ";
+        this.fragmentShader = "\n  precision mediump float;\n  uniform vec4 u_FragColor;\n  void main() {\n    gl_FragColor = u_FragColor;\n  }\n  ";
+        this.g_points = [];
+        this.g_colors = [];
         this.canvasElement = document.querySelector('#webgl-canvas');
         if (!this.canvasElement) {
             console.log('Failed to get canvas element');
@@ -13,6 +16,9 @@ var Scene = /** @class */ (function () {
             console.log('Failed to create program');
             return;
         }
+        this.canvasElement.onmousedown = function (ev) {
+            _this.onClick(ev, _this.gl, _this.canvasElement, _this.a_Position, _this.u_FragColor);
+        };
         // Set the color
         this.gl.clearColor(0.0, 1.0, 1.0, 1.0);
         // Clear canvas
@@ -27,8 +33,18 @@ var Scene = /** @class */ (function () {
             console.log('Failed to create program');
             return false;
         }
-        var a_Position = this.gl.getAttribLocation(program, 'a_Position');
-        this.gl.vertexAttrib3f(a_Position, 0.0, 0.0, 0.0);
+        // Get attribute location
+        this.a_Position = this.gl.getAttribLocation(program, 'a_Position');
+        this.u_FragColor = this.gl.getUniformLocation(program, 'u_FragColor');
+        var a_PointSize = this.gl.getAttribLocation(program, 'a_PointSize');
+        // Declare new position
+        var position = new Float32Array([0.5, 0.0, 0.0, 1.0]);
+        var color = [0.0, 0.5, 0.0, 1.0];
+        var pointSize = 14;
+        // Set new position
+        this.gl.vertexAttrib4fv(this.a_Position, position);
+        this.gl.vertexAttrib1f(a_PointSize, pointSize);
+        this.gl.uniform4fv(this.u_FragColor, color);
         // Use program
         this.gl.useProgram(program);
         return true;
@@ -80,6 +96,34 @@ var Scene = /** @class */ (function () {
             return null;
         }
         return shader;
+    };
+    Scene.prototype.onClick = function (ev, gl, canvas, a_Position, u_FragColor) {
+        var x = ev.clientX;
+        var y = ev.clientY;
+        var rect = ev.target.getBoundingClientRect();
+        x = (x - rect.left - canvas.width / 2) / (canvas.width / 2);
+        y = (canvas.height / 2 - (y - rect.top)) / (canvas.height / 2);
+        // Store the coordinates to g_points array
+        this.g_points.push([x, y]);
+        console.log(this.g_points);
+        // Clear canvas
+        this.gl.clear(this.gl.COLOR_BUFFER_BIT);
+        if (x >= 0.0 && y >= 0.0) {
+            this.g_colors.push([1.0, 0.0, 0.0, 1.0]);
+        }
+        else if (x < 0.0 && y < 0.0) {
+            this.g_colors.push([0.0, 1.0, 0.0, 1.0]);
+        }
+        else {
+            this.g_colors.push([1.0, 1.0, 1.0, 1.0]);
+        }
+        for (var i = 0; i < this.g_points.length; i += 2) {
+            var xy = this.g_points[i];
+            var rgba = this.g_colors[i];
+            this.gl.vertexAttrib3f(a_Position, xy[0], xy[1], 0.0);
+            this.gl.uniform4fv(this.u_FragColor, rgba);
+            this.gl.drawArrays(this.gl.POINTS, 0, 1);
+        }
     };
     return Scene;
 }());
--- chapter-2/index.ts
+++ chapter-2/index.ts
@@ -3,17 +3,24 @@ class Scene {
   private gl: WebGL2RenderingContext;
   private vertexShader: string = `
   attribute vec4 a_Position;
+  attribute float a_PointSize;
 
   void main() {
     gl_Position = a_Position;
-    gl_PointSize = 10.0;
+    gl_PointSize = a_PointSize;
   }
   `;
   private fragmentShader: string = `
+  precision mediump float;
+  uniform vec4 u_FragColor;
   void main() {
-    gl_FragColor = vec4(1.0,0.0,1.0,1.0);
+    gl_FragColor = u_FragColor;
   }
   `;
+  private a_Position: number;
+  private u_FragColor: WebGLUniformLocation;
+  private g_points: Array<number[]> = [];
+  private g_colors: Array<number[]> = [];
 
   constructor() {
     this.canvasElement = document.querySelector('#webgl-canvas');
@@ -35,6 +42,16 @@ class Scene {
       console.log('Failed to create program');
       return;
     }
+
+    this.canvasElement.onmousedown = (ev) => {
+      this.onClick(
+        ev,
+        this.gl,
+        this.canvasElement,
+        this.a_Position,
+        this.u_FragColor
+      );
+    };
 
     // Set the color
     this.gl.clearColor(0.0, 1.0, 1.0, 1.0);
@@ -59,8 +76,21 @@ class Scene {
       return false;
     }
 
-    const a_Position = this.gl.getAttribLocation(program, 'a_Position');
-    this.gl.vertexAttrib3f(a_Position, 0.0, 0.0, 0.0);
+    // Get attribute location
+    this.a_Position = this.gl.getAttribLocation(program, 'a_Position');
+    this.u_FragColor = this.gl.getUniformLocation(program, 'u_FragColor');
+
+    const a_PointSize = this.gl.getAttribLocation(program, 'a_PointSize');
+
+    // Declare new position
+    const position = new Float32Array([0.5, 0.0, 0.0, 1.0]);
+    const color = [0.0, 0.5, 0.0, 1.0];
+    const pointSize = 14;
+
+    // Set new position
+    this.gl.vertexAttrib4fv(this.a_Position, position);
+    this.gl.vertexAttrib1f(a_PointSize, pointSize);
+    this.gl.uniform4fv(this.u_FragColor, color);
 
     // Use program
     this.gl.useProgram(program);
@@ -140,6 +170,46 @@ class Scene {
     }
 
     return shader;
+  }
+
+  private onClick(
+    ev: MouseEvent,
+    gl: WebGLRenderingContext,
+    canvas: HTMLCanvasElement,
+    a_Position: number,
+    u_FragColor: WebGLUniformLocation
+  ): void {
+    let x = ev.clientX;
+    let y = ev.clientY;
+
+    const rect: DOMRect = (ev.target as any).getBoundingClientRect() as DOMRect;
+
+    x = (x - rect.left - canvas.width / 2) / (canvas.width / 2);
+    y = (canvas.height / 2 - (y - rect.top)) / (canvas.height / 2);
+
+    // Store the coordinates to g_points array
+    this.g_points.push([x, y]);
+
+    console.log(this.g_points);
+    // Clear canvas
+    this.gl.clear(this.gl.COLOR_BUFFER_BIT);
+
+    if (x >= 0.0 && y >= 0.0) {
+      this.g_colors.push([1.0, 0.0, 0.0, 1.0]);
+    } else if (x < 0.0 && y < 0.0) {
+      this.g_colors.push([0.0, 1.0, 0.0, 1.0]);
+    } else {
+      this.g_colors.push([1.0, 1.0, 1.0, 1.0]);
+    }
+
+    for (let i = 0; i < this.g_points.length; i += 2) {
+      const xy = this.g_points[i];
+      const rgba = this.g_colors[i];
+      this.gl.vertexAttrib3f(a_Position, xy[0], xy[1], 0.0);
+      this.gl.uniform4fv(this.u_FragColor, rgba);
+
+      this.gl.drawArrays(this.gl.POINTS, 0, 1);
+    }
   }
 }
 
